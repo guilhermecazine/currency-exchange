33315224b13053614fdec06d08ca88e040dcd8d1
guilhermecazine <guilhermecazine50@gmail.com>
2024-03-04 20:31:50 +0000

    NavBar created

---
+++ src/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">Currency Converter</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Converter</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">Exchange Table</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
